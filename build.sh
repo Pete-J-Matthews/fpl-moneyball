9e24556bf74437f88ff75af666161b187e142e8f
Pete Matthews <pete.matthews@beauhurst.com>
2024-11-20 16:04:23 +0000

    update requirements file

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-conda install -r environment.yml
+# Install Python dependencies
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
